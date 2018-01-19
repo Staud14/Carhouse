25fef9bad7023315373be58664d52d7a268482f8
chriss0612 <chrisi.schrefl@gmail.com>
2018-01-19 15:52:48 +0100

    Destroying Things XD

--- car_Christian_Schrefl.c
+++ car_Christian_Schrefl.c
@@ -1,5 +1,26 @@
 #include "car_Christian_Schrefl.h"
 
-
-
-
+void loopTimeTable(TIMETABLE *current)
+{
+    ///Looping through the Time Table
+    while(current!=NULL)
+    {
+        if(current->next->funktion()==KILL) //Deleting the function from list
+        {
+            TIMETABLE *temp;
+            temp=current->next;
+            if(current==current->next)
+            {
+                current=NULL;
+                free(temp);
+                return 0;
+            }
+            else
+            {
+                current->next=current->next->next;
+            }
+            free(temp);
+        }
+        current=current->next;
+    }
+}
--- car_Christian_Schrefl.h
+++ car_Christian_Schrefl.h
@@ -2,5 +2,18 @@
 #define CAR_CHRISTIAN_SCHREFL_H_INCLUDED
 
 
+//Return Value Encodings
+#define KEEP 1
+#define KILL 0
+
+
+typedef struct TimeTable
+{
+    int (*funktion)();          //Pointer to a Function returning an integer
+    struct TimeTable *next;     //Next Element on the circular List
+} TIMETABLE;
+
+
+void loopTimeTable(TIMETABLE *current);
 
 #endif // CAR_CHRISTIAN_SCHREFL_H_INCLUDED
